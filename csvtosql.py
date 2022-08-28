c042719c19e57a529856534c4e06da556fe067ad
Aaditya Rengarajan <aadityarenga@gmail.com>
2022-08-29 02:18:17 +0530

    first commit

--- csvtosql.py
+++ csvtosql.py
@@ -1,21 +1,19 @@
+#!/usr/bin/env python
+
+
 import mysql.connector
 import os
-
 DB_NAME=''
 for i in os.listdir():
 	if i.endswith('.csv'):
 		DB_NAME=i.replace('.csv','')
 		break
-
 conn = mysql.connector.connect(user='root', password='toor')
-
 crsr = conn.cursor()
-
 def cmdexec(lst):
 	for i in lst:
 		crsr.execute(i.replace('\n',''))
 		conn.commit()
-
 def listasstr(lst):
 	temp=''
 	for i in lst.split(','):
@@ -23,12 +21,9 @@ def listasstr(lst):
 		if i!=lst.split(',')[-1]:
 			temp+=', '
 	return temp.replace('\n','')
-
 with open(f'{DB_NAME}.csv') as f:
 	DB_HDGS=f.readlines()[0]
-
 dbcreate=f'CREATE TABLE {DB_NAME}TABLE ('
-
 creationappend=DB_HDGS.split(',')
 try:
 	creationappend=creationappend.remove(' ')
@@ -38,7 +33,6 @@ try:
 	creationappend=creationappend.remove(' ')
 except:
 	pass
-
 for hdg in creationappend:
 	for j in hdg:
 		dbcreate+=j
@@ -46,30 +40,21 @@ for hdg in creationappend:
 	if hdg!=creationappend[-1]:
 		dbcreate+=', '
 dbcreate+=');'
-
 try:
 	cmdexec([f'CREATE DATABASE {DB_NAME};'])
 except:
 	cmdexec([f'DROP DATABASE {DB_NAME};',f'CREATE DATABASE {DB_NAME};'])
-
-
 tablecreator=[f'USE {DB_NAME};',dbcreate]
-
 commands=[]
-
 with open(f'{DB_NAME}.csv') as f:
 	first=True
 	for rec in f.readlines():
 		if first==False:
 			commands.append(f"INSERT INTO {DB_NAME}TABLE ( {(DB_HDGS)} ) VALUES ( {listasstr(rec)} );")
 		first=False
-
 cmdexec(tablecreator)
-
 cmdexec(commands)
-
 crsr.execute(f"SELECT * FROM {DB_NAME}table")
 for i in crsr:
 	print(i)
-
 conn.close()
